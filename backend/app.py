ba168a5000eb1fbf369b025b649114ecbab7cfca
R3VANEK <janek2000-2000@wp.pl>
2025-03-26 11:42:59 +0100

    test6

--- backend/app.py
+++ backend/app.py
@@ -2,9 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index() -> str:
-    return 'Hello World'
+    return "Hello World"
 
 
 if __name__ == "__main__":
