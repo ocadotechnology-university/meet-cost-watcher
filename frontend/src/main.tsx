f8ae75ffcb2675e6784cb286495b9adfb30adce2
Autimus <akcja53@gmail.com>
2025-04-09 22:45:17 +0200

    Login pages created.  No functionality. React-router added.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './index.css'
 import App from './App.tsx'
+import LoginPage from './login.tsx'
+import SingleTokenPage from './login_single_token.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/single_token" element={<SingleTokenPage />} />
+      </Routes>
+    </Router>
   </StrictMode>,
 )
