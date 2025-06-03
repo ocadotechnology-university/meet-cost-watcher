2d7ca940470fa99608f40e349a30b2182e956c62
Jan Napieralski <jan.napieralski.torun@wp.pl>
2025-06-03 20:45:23 +0200

    CRUD of users
    * CRUD of users
    
    * delete user from meetings
    
    * cleaned

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,7 +1,9 @@
 from .meetings import api as meeting_ns
 from .costs import api as costs_ns
+from .users import api as users_ns
 
 
 def register_namespaces(api):
     api.add_namespace(meeting_ns, path="/meetings")
     api.add_namespace(costs_ns, path="/costs")
+    api.add_namespace(users_ns, path="/users")
